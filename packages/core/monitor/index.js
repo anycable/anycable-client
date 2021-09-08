d93c50eb588eb3653026e3f333186b177de6f015
TL <tienlx@gmail.com>
2021-09-08 18:53:26 +0700

    fix: Possible unhandled promise rejection

--- packages/core/monitor/index.js
+++ packages/core/monitor/index.js
@@ -67,7 +67,7 @@ export class Monitor {
     this.cancelReconnect()
 
     this.state = 'pending_connect'
-    this.target.connect()
+    this.target.connect().catch(err => this.logger.info("Failed at reconnecting: " + err))
 
     return true
   }
