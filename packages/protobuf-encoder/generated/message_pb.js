c4987c0a7577ff14f29b922ccbc27f779fc31a96
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-09-29 15:22:58 +0300

    fix(protobuf): add .js to protobuf import

--- packages/protobuf-encoder/generated/message_pb.js
+++ packages/protobuf-encoder/generated/message_pb.js
@@ -1,5 +1,5 @@
 /*eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars*/
-import $protobuf from "protobufjs/minimal";
+import $protobuf from "protobufjs/minimal.js";
 
 // Common aliases
 const $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;
