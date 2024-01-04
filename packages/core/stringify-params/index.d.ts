3ad60cc15836c3d16db730c3af1114a5c6aeb546
Artur Petrov <ardecvz@gmail.com>
2024-01-04 14:35:19 +0300

    Omit `undefined` within Channels' params to fix invalid JSON serialization
    It's intended to mimic the ActionCable behavior when using `JSON.stringify`.
    
    Also, extend the type signature to allow `null` values.

--- packages/core/stringify-params/index.d.ts
+++ packages/core/stringify-params/index.d.ts
@@ -1,3 +1,3 @@
-type Params = { [token: string]: boolean | number | string }
+type Params = { [token: string]: boolean | number | string | null | undefined }
 
 export function stringifyParams(params: Params | void | null): string
